3e05a61767fcf77ca98cdabd8d53d6264f248593
Ambro <ambrosetall@gmail.com>
2022-02-09 12:17:59 +0300

    sec commit

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ dotenv.config()
 
 mongoose.connect(
     process.env.MONGO_URL
-    // "mongodb+srv://Ambrose:Ambro12345@cluster0.tlpb9.mongodb.net/commerce?retryWrites=true&w=majority"
 ).then(()=> console.log('DB connected'))
 .catch(err =>{
     console.log(err);
